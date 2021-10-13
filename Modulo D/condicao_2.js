abc3111e41c4f6984de1a22f1f5c5b18490726e9
olinad1993 <olinad1993@gmail.com>
2021-10-13 15:50:13 -0300

    Trabalhando com Condições

---
+++ Modulo D/condicao_2.js
@@ -0,0 +1,10 @@
+// CONDIÇÃO COMPOSTA
+var pais = 'EUA' 
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil') {
+    console.log('Você é Brasileiro')
+
+} else {
+    console.log('Você é Estrangeiro!')
+
+}
---
+++ Modulo D/condicoes_1.js
@@ -0,0 +1,9 @@
+// CONDIÇÃO SIMPLES
+var velocidade = 80
+console.log(`A velocidade do carro é ${velocidade}Km/h`)
+
+if(velocidade > 60) {
+    console.log('Você ultrapassou a velocidade. MULTADO!')
+}else {
+    console.log('Boa Viagem! Dirija com cuidado.')
+}

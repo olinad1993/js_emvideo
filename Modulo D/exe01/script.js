106cb1e290936e5caee6c77bec151a996e5be3f4
olinad1993 <olinad1993@gmail.com>
2021-10-18 17:01:01 -0300

    Primeiro exercicio do modulo D

---
+++ Modulo D/exe01/script.js
@@ -0,0 +1,25 @@
+function carregar() {
+    var msg = window.document.getElementById("msg")
+    var img = window.document.getElementById("imagem")
+    var data = new Date ()
+    var hora = data.getHours ()
+    // var hora = 10
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA
+        img.src = 'manha.png'
+        document.body.style.background = '#c99f90' // alterar cor conforme muda a hora do dia.
+    
+    } else if (hora >= 12 && hora <= 18){
+            // BOA TARDE
+            img.src = 'tarde.png'
+            document.body.style.background = '#D27A67'
+
+    } else {
+    // BOA NOITE
+    img.src = 'noite.png'
+    document.body.style.background = '#15354a'
+    }
+    
+}
+

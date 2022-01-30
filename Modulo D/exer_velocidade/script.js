f2900c31e0fa3fd419955366cda0cd8b2239f611
Danilo Ferreira <olinad1993@gmail.com>
2022-01-30 15:32:48 -0300

    identação

--- Modulo D/exer_velocidade/script.js
+++ Modulo D/exer_velocidade/script.js
@@ -6,9 +6,7 @@ function calcular() {
     result.innerHTML = `<p> Sua velocidade atual é de <strong> ${vel_atual} KM/h </strong> </p> `
 
     if (vel_atual > vel_max) {
-        result.innerHTML +=
-            `<p> <strong>VOCÊ FOI MULTADO!! </strong> <u>Motivo:</u> A cima da velocidade máxima.`
-
+        result.innerHTML += `<p> <strong>VOCÊ FOI MULTADO!! </strong> <u>Motivo:</u> A cima da velocidade máxima.`
 
     } else {
         result.innerHTML += '<p>Dirija sempre com cinto de segurança!</p>'

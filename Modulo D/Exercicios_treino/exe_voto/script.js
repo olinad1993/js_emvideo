1f506620fbb7281f1befb29f829a1537a71646dc
Danilo Ferreira <85970991+olinad1993@users.noreply.github.com>
2022-01-31 12:17:06 -0300

    Correção do Return

--- Modulo D/Exercicios_treino/exe_voto/script.js
+++ Modulo D/Exercicios_treino/exe_voto/script.js
@@ -2,13 +2,16 @@ function verificar() {
     let txtidade = document.getElementById('txtidade')
     let result = document.getElementById('result')
     let idade = Number(txtidade.value)
+
+    if(txtidade.value == 0){
+      alert('Idade não informada!')
+      txtidade.focus();
+      return;
+    }
+    
     result.innerHTML = `<p>Você tem <strong>${idade} anos.</strong> </p>` 
   
-  if(txtidade.value == 0){
-    alert('Idade não informada!')
-    txtidade.focus();
-    return;
-  }
+ 
     if (idade < 16) {
       result.innerHTML += `<strong>Você ainda não vota!</strong>`
   } else if (idade < 18 || idade > 65) {
